f9a9f11d2e89cfc4d2159f5cc2909d0b83a79d94
bettyassefa <bettyassefa26@gmail.com>
2021-08-31 11:19:43 -0700

    put char

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,17 @@
 #include "main.h"
-
 /**
-* main - Print the word "Holberton"
-*
-* Return: Always 0 (Success)
-*/
-
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
 int main(void)
 {
+	char c[] = "Holberton";
 	int i;
-	char p[] = "Holberton";
 
 	for (i = 0; i < 9; i++)
-		{
-		_putchar(p[i]);
-		}
-		_putchar('\n');
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
