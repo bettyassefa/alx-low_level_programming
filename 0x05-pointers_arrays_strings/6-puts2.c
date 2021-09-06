134505321f87827b65863182601ae4964afe11e2
bettyassefa <bettyassefa26@gmail.com>
2021-09-06 12:45:39 -0700

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,29 @@
+#include <stdio.h>
 #include "main.h"
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
-*/
-int main(void)
+ *puts2 - print pair values.
+ *@str: value to be evaluate.
+ *Return: not.
+ */
+void puts2(char *str)
 {
-	char *str;
+	int len = 0;
+	int l = 0;
+	char *y = str;
+	int z;
 
-	str = "0123456789";
-	puts2(str);
-	return (0);
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for (z = 0 ; z <= l ; z++)
+	{
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
+	}
+	_putchar('\n');
 }
