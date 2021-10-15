8c2df011ed09c582cc49c5c639ffcf8b4d1339b5
bettyassefa <bettyassefa26@gmail.com>
2021-10-14 20:47:32 -0700

    more_singly_linked_lists adva

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,14 +1,29 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
-  * reverse_listint - ...
-  * @head: ...
-  *
-  * Return: ...
-  */
+ * reverse_listint - reverse linked list
+ * @head: pointer to head pointer of linked list
+ * Return: pointer to first node of reversed list
+ */
+
 listint_t *reverse_listint(listint_t **head)
 {
-	head++;
-	return (0);
+	listint_t *prev, *next;
+
+	/* account for no ptr and empty list */
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	/* iterate thorugh list to reverse linking */
+	prev = NULL;
+	while (*head != NULL)
+	{
+		next = (*head)->next; /* keep track of next node to move to */
+		(*head)->next = prev; /* link current node's ptr to prev node */
+		prev = *head; /* update previous node to be current node */
+		*head = next; /* move current node to next node */
+	}
+	(*head) = prev; /* unlink last element from null and point backwards */
+
+	return (*head);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
 #ifndef _S_LINK_LISTS_
 #define _S_LINK_LISTS_
+#include <stddef.h>
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
@@ -27,5 +31,8 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
